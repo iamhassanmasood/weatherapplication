557d1886d18c51aa3a27a84309ab57d88607b67b
iamhassanmasood <iamhassanmasood@gmail.com>
2019-01-07 18:35:01 +0500

    Checking Again

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,7 +3,10 @@ class Weather extends Component{
     render(){
         return(
             <div>
-                <p>Weather Component</p>
+               <p>Location: {this.props.city} {this.props.country}</p> 
+                <p>Temperature: {this.props.temperature}</p>
+                <p>Humidity: {this.props.humidity}</p>
+                <p>Conditions: {this.props.description}</p>
             </div>
 
         );

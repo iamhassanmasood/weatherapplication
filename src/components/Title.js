55298a61371d90cccc631820ba538d6d23952efc
iamhassanmasood <iamhassanmasood@gmail.com>
2019-01-07 16:19:02 +0500

    FIrst

---
+++ src/components/Title.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class Title extends Component{
+    render(){
+        return(
+            <div>
+                <h1>
+                    Weather Finder
+                </h1>
+                <p>Find Out Temperature</p>
+            </div>
+
+        );
+    }
+}
+
+export default Title;

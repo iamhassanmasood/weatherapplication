08600bab5b72bb955f1e87b59e23c0bff4a4360b
iamhassanmasood <iamhassanmasood@gmail.com>
2019-01-07 19:34:41 +0500

    make one stateless component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,16 +1,11 @@
-import React, { Component } from 'react';
-class Weather extends Component{
-    render(){
-        return(
+import React from 'react';
+const Weather=(props)=>(
             <div>
-               {this.props.city && this.props.country && <p>Location: {this.props.city} {this.props.country}</p>} 
-               {this.props.temperature && <p>Temperature: {this.props.temperature}</p> }
-               {this.props.humidity && <p>Humidity: {this.props.humidity}</p> }
-               {this.props.description && <p>Conditions: {this.props.description}</p>}
+               {props.city && props.country && <p>Location: {props.city} {props.country}</p>} 
+               {props.temperature && <p>Temperature: {props.temperature}</p> }
+               {props.humidity && <p>Humidity: {props.humidity}</p> }
+               {props.description && <p>Conditions: {props.description}</p>}
             </div>
-
-        );
-    }
-}
+)
 
 export default Weather;
